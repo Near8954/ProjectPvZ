5cd34580e43607716b53cc777d3c21659a2c2c4e
Near8954 <roma.baldin@gmail.com>
2022-12-19 23:03:00 +0500

    start

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Test")
+import pygame
+
+if __name__ == '__main__':
+    pygame.init()
+    pygame.display.set_caption('PvZ')
+    size = width, height = 800, 600
+    screen = pygame.display.set_mode(size)
+
+    running = True
+    v = 5  # пикселей в секунду
+    r = 0
+    fps = 60
+    is_circle = False
+    clock = pygame.time.Clock()
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = event.pos
+                print(f'click:{x, y}')
+        screen.fill((0, 0, 0))
+        clock.tick(fps)
+        pygame.display.flip()
+    pygame.quit()
