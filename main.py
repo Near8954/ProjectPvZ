fbad96efc8b7d34b8f5ecf092b27b37f84299292
Near8954 <roma.baldin@gmail.com>
2022-12-23 16:26:28 +0500

    Added start menu

--- main.py
+++ main.py
@@ -1,9 +1,61 @@
 import pygame
+import os
+import sys
+
+
+size = WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode(size)
+FPS = 50
+clock = pygame.time.Clock()
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+def start_screen():
+    intro_text = ["ЗАСТАВКА", "",
+                  "Правила игры",
+                  "Если в правилах несколько строк,",
+                  "приходится выводить их построчно"]
+
+    fon = pygame.transform.scale(load_image('temp_start_menu.png'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+        
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    terminate()
+                elif event.type == pygame.KEYDOWN or \
+                        event.type == pygame.MOUSEBUTTONDOWN:
+                    return  # начинаем игру
+            pygame.display.flip()
+            clock.tick(FPS)
+
+
 
 if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption('PvZ')
-    size = width, height = 800, 600
+    size = WIDTH, HEIGHT
     screen = pygame.display.set_mode(size)
 
     running = True
@@ -12,10 +64,11 @@ if __name__ == '__main__':
     fps = 60
     is_circle = False
     clock = pygame.time.Clock()
+    start_screen()
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                terminate()            
             if event.type == pygame.MOUSEBUTTONDOWN:
                 x, y = event.pos
                 print(f'click:{x, y}')
